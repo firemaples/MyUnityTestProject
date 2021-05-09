f23dc88a3859c5fa5466708e869a663b00514a34
firemaples <firemaples@gmail.com>
2021-05-09 21:55:03 +0800

    Add script to controll the position of the bird and turn the bird to red while the mouse down

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().isKinematic = true;
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+    }
+
+    private void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(mousePosition.x, mousePosition.y, transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
